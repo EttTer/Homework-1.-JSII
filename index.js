a310bc7734fe7341f339165bb5228f9be01dc99a
EttTer <tereza.ettlerova@gmail.com>
2022-10-19 09:27:09 +0200

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('funguju!');
+const Task = (props) => {
+
+    const {done, due, id, name} = props;
+return `<div class="todo__tasks">
+<div class="task">
+<div class="task__body">
+  <div class="task__name">${name}</div>
+  <div class="task__due">${due}</div>
+</div>
+<div class="task__done">${done}</div>
+</div>`;
+};
+
+
+
+const renderTasks = (tasks) => {
+    const taskElm = document.querySelector(".task");
+    taskElm.innerHTML= tasks
+    .map((t) => Task(t))
+    .join("");
+};
+
+
+
+/* Vypíšeme všechny pole do konzole
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
+.then((response)=> {
+    return response.json();
+})
+.then((data)=> console.log(data));*/
+
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
+.then((response)=> {
+    return response.json();
+})
+.then(renderTasks);
